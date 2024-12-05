5461a92279a06398597500eb7e9c5c4d160a3a01
AnaelleChiles <118526850+AnaelleChiles@users.noreply.github.com>
2024-12-05 22:38:07 +0100

    pas fini réparer fonction

---
+++ global.js
@@ -0,0 +1,53 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function show_image() {
+    var img = document.getElementById("picture");
+    var division = Math.random() * 5;
+    img.width = img.width;
+    img.height = img.height;
+
+    var taille = (document.body.clientHeight - img.height) * Math.random()
+
+    console.log(taille)
+
+    // set the position
+    img.style.position = 'absolute';
+    img.style.top = document.body.clientHeight * Math.random() + 'px';
+    img.style.left = document.body.clientWidth * Math.random() + 'px';
+
+    document.body.appendChild(img);
+}
+
+function rotateAnimation(degrees) {
+    var elem = document.getElementById("picture");
+    if (navigator.userAgent.match("Chrome")) {
+        elem.style.WebkitTransform = "rotate(" + degrees + "deg)";
+    } else if (navigator.userAgent.match("Firefox")) {
+        elem.style.MozTransform = "rotate(" + degrees + "deg)";
+    } else if (navigator.userAgent.match("MSIE")) {
+        elem.style.msTransform = "rotate(" + degrees + "deg)";
+    } else if (navigator.userAgent.match("Opera")) {
+        elem.style.OTransform = "rotate(" + degrees + "deg)";
+    } else {
+        elem.style.transform = "rotate(" + degrees + "deg)";
+    }
+}
+
+show_image();
+
+document.getElementById('picture').addEventListener('click', async () => {
+    var degrees = 1;
+    while (degrees != 0) {
+        setTimeout(rotateAnimation(degrees), 5000);
+        degrees++;
+        await sleep(5)
+        if (degrees == 361){
+            degrees = 0;
+        }
+    }
+
+});
+
+
